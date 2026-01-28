0fe2b50b95863aac56733efc2b1d948a0f6fc696
R0SHANI2525 <116300017+R0SHANI2525@users.noreply.github.com>
2026-01-28 13:28:19 +0530

    Update test_script.py

--- glue_scripts/test_script.py
+++ glue_scripts/test_script.py
@@ -1,1 +1,20 @@
-print("Glue script upload test successful")
+import sys
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+
+# Initialize Spark and Glue context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+print("===================================")
+print("AWS GLUE JOB STARTED SUCCESSFULLY")
+print("Glue version is working")
+print("IAM Role attached correctly")
+print("S3 access is available")
+print("===================================")
+
+# Simple Spark operation (to prove Spark works)
+data = [("Roshani", "Terraform"), ("AWS", "Glue")]
+df = spark.createDataFrame(data, ["Name", "]()
