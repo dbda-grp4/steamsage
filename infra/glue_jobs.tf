6fae6c30c88d6f6c104887a97666d2c181bcd565
R0SHANI2525 <116300017+R0SHANI2525@users.noreply.github.com>
2026-01-30 16:05:59 +0530

    Update glue_jobs.tf

--- infra/glue_jobs.tf
+++ infra/glue_jobs.tf
@@ -1,6 +1,3 @@
-########################################
-# APPLICATIONS ETL JOB
-########################################
 resource "aws_glue_job" "applications" {
   name     = "tf-steam-applications-etl"
   role_arn = aws_iam_role.glue_service_role.arn
@@ -15,61 +12,13 @@ resource "aws_glue_job" "applications" {
   worker_type       = "G.1X"
   number_of_workers = 2
   timeout           = 60
-}
+  max_retries       = 1
 
-########################################
-# REVIEWS ETL JOB
-########################################
-resource "aws_glue_job" "reviews" {
-  name     = "tf-steam-reviews-etl"
-  role_arn = aws_iam_role.glue_service_role.arn
-
-  command {
-    name            = "glueetl"
-    script_location = "s3://steam-glue-roshani-2026/glue-scripts/reviews_transformations.py"
-    python_version  = "3"
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-glue-datacatalog"          = "true"
+    "--job-bookmark-option"              = "job-bookmark-enable"
+    "--TempDir"                          = "s3://steam-glue-roshani-2026/temp/"
   }
-
-  glue_version      = "4.0"
-  worker_type       = "G.1X"
-  number_of_workers = 2
-  timeout           = 60
-}
-
-########################################
-# DIMENSIONS ETL JOB
-########################################
-resource "aws_glue_job" "dimensions" {
-  name     = "tf-steam-dimensions-etl"
-  role_arn = aws_iam_role.glue_service_role.arn
-
-  command {
-    name            = "glueetl"
-    script_location = "s3://steam-glue-roshani-2026/glue-scripts/join_bridges.py"
-    python_version  = "3"
-  }
-
-  glue_version      = "4.0"
-  worker_type       = "G.1X"
-  number_of_workers = 2
-  timeout           = 60
-}
-
-########################################
-# MASTERDATA ETL JOB
-########################################
-resource "aws_glue_job" "masterdata" {
-  name     = "tf-steam-masterdata-etl"
-  role_arn = aws_iam_role.glue_service_role.arn
-
-  command {
-    name            = "glueetl"
-    script_location = "s3://steam-glue-roshani-2026/glue-scripts/masterdata.py"
-    python_version  = "3"
-  }
-
-  glue_version      = "4.0"
-  worker_type       = "G.1X"
-  number_of_workers = 2
-  timeout           = 60
 }
