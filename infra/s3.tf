673c5b1310920a30ff18cd4058a3ccd048147132
R0SHANI2525 <116300017+R0SHANI2525@users.noreply.github.com>
2026-01-30 16:41:23 +0530

    Add files via upload

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,4 @@
+# S3 bucket for Glue scripts and data
 resource "aws_s3_bucket" "glue_bucket" {
   bucket = "steam-glue-roshani-2026"
 
@@ -5,8 +6,22 @@ resource "aws_s3_bucket" "glue_bucket" {
     Project = "Steam Data Pipeline"
     Owner   = "Terraform"
   }
+}
 
-  lifecycle {
-    prevent_destroy = true
-  }
+# Create folder for Glue scripts
+resource "aws_s3_object" "glue_scripts_folder" {
+  bucket = aws_s3_bucket.glue_bucket.id
+  key    = "glue-scripts/"
+}
+
+# Create folder for raw data
+resource "aws_s3_object" "raw_folder" {
+  bucket = aws_s3_bucket.glue_bucket.id
+  key    = "raw/"
+}
+
+# Create folder for curated data
+resource "aws_s3_object" "curated_folder" {
+  bucket = aws_s3_bucket.glue_bucket.id
+  key    = "curated/"
 }
