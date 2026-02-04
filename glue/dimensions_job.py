8ad1103f5b98d4f351fdec0f94ef1f25a7e0586a
Slyther12 <amandeepjaiswar@gmail.com>
2026-02-04 19:38:19 +0530

    Update dimensions_job.py

--- glue/dimensions_job.py
+++ glue/dimensions_job.py
@@ -39,7 +39,7 @@ spark = glueContext.spark_session
 app_devs_path       = f"{RAW_BASE}/application_developers.csv"
 developers_path     = f"{RAW_BASE}/developers.csv"
 
-app_publishers_path = f"{RAW_BASE}/application_publishers.csv"
+app_pubs_path = f"{RAW_BASE}/application_publishers.csv"
 publishers_path     = f"{RAW_BASE}/publishers.csv"
 
 app_genres_path     = f"{RAW_BASE}/application_genres.csv"
@@ -159,3 +159,4 @@ app_categories_rows_df.write.mode("overwrite").parquet(out_categories)
 app_platforms_rows_df.write.mode("overwrite").parquet(out_platforms)
 
 print("Dimensions job completed successfully.")
+
