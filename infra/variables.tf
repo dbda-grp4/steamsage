f949133615703eb4a87c5900c61d35bbb03f6fe7
Slyther12 <amandeepjaiswar@gmail.com>
2026-02-05 13:43:07 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -23,7 +23,7 @@ variable "environment" {
 # S3
 # -----------------------------
 variable "data_bucket_name" {
-  description = "S3 bucket for raw, silver, gold data and scripts"
+  description = "S3 bucket for raw, silver data and scripts"
   type        = string
 }
 
@@ -67,3 +67,4 @@ variable "github_repo" {
   description = "The GitHub repository (Org/Name) to trust. passed automatically by GitHub Actions."
   type        = string
 }
+
